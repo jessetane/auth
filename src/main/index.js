ad3a77a0652dca97f32a331ba203195b3ea67666
Jesse Tane <jesse.tane@gmail.com>
2015-05-23 15:28:22 -0400

    render client name

--- src/main/index.js
+++ src/main/index.js
@@ -6,10 +6,20 @@ var AuthServer = require('../server')
 
 function AuthMainView () {
   this.el = hg(template)
+  this.render = this.render.bind(this)
 }
 
 AuthMainView.prototype.show = function () {
   if (!this.authServer) {
     this.authServer = new AuthServer(window.parent)
+    window.addEventListener('message', this.render);
+  }
+}
+
+AuthMainView.prototype.render = function () {
+  if (this.authServer.remote.origin) {
+    hg(this.el, {
+      '#origin': this.authServer.remote.origin
+    })
   }
 }
